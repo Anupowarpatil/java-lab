0e6459940fa867b43d97b44b14ce379c50ff801d
Purva kiran powar patil <109520470+Anupowarpatil@users.noreply.github.com>
2024-08-22 10:19:07 +0530

    simple-Exception-handling

---
+++ Execption.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Execption{
+	public static void main(String args[])
+	{
+		System.out.println("Program is started");
+		int a=10,b=0,c;
+		try{
+			c=a/b;
+			System.out.println(c);
+		}
+		catch(Exception e){
+			System.out.println("Number is not divisible by 0");
+		}
+		System.out.println("Program is ended");
+
+	}
+}
