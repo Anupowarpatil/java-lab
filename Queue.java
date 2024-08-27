217c4ab54c9d2c9002a576b96ec7fc7674d3f180
Purva kiran powar patil <109520470+Anupowarpatil@users.noreply.github.com>
2024-08-27 23:30:18 +0530

    Create Queue.java

---
+++ Queue.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueCreation {
+    public static void main(String args[]) {
+        
+        // Create a Queue of Integers using LinkedList
+        Queue<Integer> queue = new LinkedList<>();
+        
+        // Displaying the Queue
+        System.out.println("Queue elements: " + queue);
+    }
+}
